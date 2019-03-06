6e1f013b77cc06937db6ad3fbf218b55dcd2b254
Ryota Kishima <>
2019-03-06 21:57:18 +0900

    テスト

---
+++ Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private const string key_IsDamage = "IsDamage";
+
+    // Use this for initialization
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            this.animator.SetBool(key_IsDamage, true);
+        }else{
+            this.animator.SetBool(key_IsDamage, false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private const string key_IsRun = "IsRun";
+    private const string key_IsAtack = "IsAtack";
+    private const string key_IsAtack2 = "IsAtack2";
+    private const string key_IsAtack3 = "IsAtack3";
+    // Use this for initialization
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.animator.SetBool(key_IsRun, true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.animator.SetBool(key_IsRun, true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.animator.SetBool(key_IsRun, true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.animator.SetBool(key_IsRun, true);
+        }else{
+            this.animator.SetBool(key_IsRun, false);
+        }
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            this.animator.SetBool(key_IsAtack, true);
+        }else{
+            this.animator.SetBool(key_IsAtack, false);
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            this.animator.SetBool(key_IsAtack2, true);
+        }else
+        {
+            this.animator.SetBool(key_IsAtack2, false);
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            this.animator.SetBool(key_IsAtack3, true);
+        }else
+        {
+            this.animator.SetBool(key_IsAtack3, false);
+        }
+    }
+}
